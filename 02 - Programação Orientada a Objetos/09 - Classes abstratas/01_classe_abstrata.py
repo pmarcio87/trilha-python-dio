4d3add40c2378a1074c69faf536d39cc13ed88bd
Paulo Marcio Mendes <pmarcio87@hotmail.com>
2024-04-25 17:50:41 -0300

    Updating the deprecated abstractproperty wrapper

--- 02 - Programação Orientada a Objetos/09 - Classes abstratas/01_classe_abstrata.py
+++ 02 - Programação Orientada a Objetos/09 - Classes abstratas/01_classe_abstrata.py
@@ -1,4 +1,4 @@
-from abc import ABC, abstractmethod, abstractproperty
+from abc import ABC, abstractmethod
 
 
 class ControleRemoto(ABC):
@@ -11,7 +11,7 @@ class ControleRemoto(ABC):
         pass
 
     @property
-    @abstractproperty
+    @abstractmethod
     def marca(self):
         pass
 
